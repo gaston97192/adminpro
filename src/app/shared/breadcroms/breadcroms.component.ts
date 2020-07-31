c536cb1e3f00da35dd560a9f7e0dff4d994ff50e
Gaston Aguirre <gastonaguirre19972@gmail.com>
2020-07-31 19:23:53 -0300

    seccion 3 - diseño

---
+++ src/app/shared/breadcroms/breadcroms.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-breadcroms',
+  templateUrl: './breadcroms.component.html',
+  styles: [
+  ]
+})
+export class BreadcromsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
